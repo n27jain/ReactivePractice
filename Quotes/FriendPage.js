f29e12009dc64e7b0d8d2960a6b53b22ba8e3e3b
n27jain <namanjain@xecoop1.xe.com>
2020-01-08 17:28:05 -0500

    Started a page navigation sequence

---
+++ Quotes/FriendPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class FriendPage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <text>
+                    Hey there sexy!
+                </text>
+                
+        
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+
